198464f71d64577ae7f89c748d7ff7f8db458123
ilyas-220 <ilyassamsudin388@gmail.com>
2026-04-25 06:38:22 +1200

    ''

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar({openCart,setOpenCart,cart,setCart}) {
 
   return (
     <div>
-      <nav className="flex justify-between py-5 px-10 bg-white  items-center fixed right-0 w-full z-10 ">
+      <nav className="flex justify-between py-5 px-10 bg-white  items-center fixed  w-full z-10 ">
         <div className="text-2xl font-bold text-orange-400">Warung Geprek</div>
         <div className="flex gap-10 hidden md:flex items-center">
           <a href="#home">Home</a>
